50177303767902d1518a8cd6c4bd39f502637a5c
UTM <18b61a05c5@nmrec.edu.in>
2025-02-01 23:20:04 -0600

    Hw#7

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import RandomButton from "/home/utm/Desktop/NewProject/vite-project/components/RandomButton"
+
+function App() {
+  return (
+    <>
+      <h1>Bible Verse Generator</h1>
+      <RandomButton/>
+    </>
+  );
+};
+
+export default App;
